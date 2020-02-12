e02c476792cc54b8e13ffcb0d39558f4884905df
Koen <koenvwel@gmail.com>
2020-02-12 10:26:22 +0100

    WIP WHO case

--- server/__init__.py
+++ server/__init__.py
@@ -7,13 +7,21 @@ nameIpMap = {}
 class ClientHandler(socketserver.StreamRequestHandler):
     def handle(self):
         print("Handling message from ", self.client_address)
-        data = self.rfile.readline().decode("utf-8")
-
+        data = self.rfile.readline().decode("utf-8").split()
         print("Received data:", data)
-        if data.startswith("HELLO-FROM"):
-            name = data[len("HELLO-FROM"):].strip()
-            nameIpMap[name] = self.client_address
-            print("name:", name, "ip:", nameIpMap[name])
+
+        if data[0] == "HELLO-FROM":
+            if(len(data) > 2):
+                self.wfile.write("BAD-RQST-BODY\n")
+            else:
+                name = data[1]
+                nameIpMap[name] = self.client_address
+                print("name:", name, "ip:", nameIpMap[name])
+                self.wfile.write("HELLO {}\n ".format(name).encode("utf-8"))
+
+        elif data[0] == "WHO":
+            print("WHO-OK {}\n".format(nameIpMap.keys()))
+            self.wfile.write("WHO-OK {}\n".format(nameIpMap.keys()))
 
 
 if __name__ == "__main__":
