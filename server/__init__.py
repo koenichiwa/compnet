4edc018c0e8fc404186388a438c11a02fc8ae6b7
Koen <koenvwel@gmail.com>
2020-02-12 11:56:13 +0100

    You know it

--- server/__init__.py
+++ server/__init__.py
@@ -1,27 +1,55 @@
 import socketserver
-import parser
-
-nameIpMap = {}
+import socket
 
 
 class ClientHandler(socketserver.StreamRequestHandler):
+    nameSockMap = {}
+
     def handle(self):
         print("Handling message from ", self.client_address)
-        data = self.rfile.readline().decode("utf-8").split()
-        print("Received data:", data)
+        thisname = ""
+        while True:
+            data = self.rfile.readline().decode("utf-8").split()
+            print("Received data:", data)
 
-        if data[0] == "HELLO-FROM":
-            if(len(data) > 2):
-                self.wfile.write("BAD-RQST-BODY\n")
-            else:
+            if len(data) == 0:
+                if thisname in self.nameSockMap:
+                    del self.nameSockMap[thisname]
+                print("EOF")
+                break
+
+            if data[0] == "HELLO-FROM":
+                if len(data) > 2:
+                    self.wfile.write("BAD-RQST-BODY\n")
+                else:
+                    thisname = data[1]
+                    if thisname in self.nameSockMap:
+                        print("name:", thisname, "is already in use")
+                        self.wfile.write("IN-USE\n".encode("utf-8"))
+                    else:
+                        self.nameSockMap[thisname] = self.request
+                        print("name:", thisname, "socket:", self.nameSockMap[thisname])
+                        self.wfile.write("HELLO {}\n".format(thisname).encode("utf-8"))
+
+            elif data[0] == "WHO":
+                if len(data) > 1:
+                    self.wfile.write("BAD-RQST-BODY\n")
+                else:
+                    print("WHO-OK {}\n".format(','.join(self.nameSockMap.keys())))
+                    self.wfile.write("WHO-OK {}\n".format(','.join(self.nameSockMap.keys())).encode("utf-8"))
+
+            elif data[0] == "SEND":
                 name = data[1]
-                nameIpMap[name] = self.client_address
-                print("name:", name, "ip:", nameIpMap[name])
-                self.wfile.write("HELLO {}\n ".format(name).encode("utf-8"))
+                if name not in self.nameSockMap:
+                    self.wfile.write("UNKNOWN\n".encode("utf-8"))
+                else:
+                    message = ' '.join(data[2:])
 
-        elif data[0] == "WHO":
-            print("WHO-OK {}\n".format(nameIpMap.keys()))
-            self.wfile.write("WHO-OK {}\n".format(nameIpMap.keys()))
+                    self.nameSockMap[name].sendall("DELIVERY {} {}\n".format(thisname, message).encode("utf-8"))
+                    self.wfile.write("SEND-OK\n".encode("utf-8"))
+
+            else:
+                self.wfile.write("BAD-RQST-HDR\n".encode("utf-8"))
 
 
 if __name__ == "__main__":
